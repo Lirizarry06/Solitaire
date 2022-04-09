b5ed6112bf401bfe911fb98cde2e764651897db7
Lirizarry06 <96934863+Lirizarry06@users.noreply.github.com>
2022-04-09 09:57:23 -0600

    Add files via upload

--- Solitaire/src/SolitaireFrame.java
+++ Solitaire/src/SolitaireFrame.java
@@ -12,36 +12,45 @@ public class SolitaireFrame {
 	private SolitairePanel sPanel;
 	
 	public SolitaireFrame() {
+		
 		//boilerplate code to set up JFrame
 		JFrame frame = new JFrame("Solitaire");
 		frame.setSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		// get a screen size and put a frame on the center of screen
+		//get a screen size and put a frame on the center of screen
 		Dimension screenSize = new Dimension(Toolkit.getDefaultToolkit().getScreenSize());
 		frame.setLocation(new Point((int)(screenSize.width/2.0 - FRAME_WIDTH/2.0), 
 						(int)(screenSize.height/2.0 - FRAME_HEIGHT/2.0 )));	
 		
-		this.sPanel = new SolitairePanel();
-		frame.add(sPanel, BorderLayout.CENTER);
-		JPanel settingsBar = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		settingsBar.setBackground(Color.GREEN);
-		JPanel timerBar = new JPanel(new BorderLayout());
-		timerBar.setBackground(Color.GREEN);
+		//set icon and title bar color
+		frame.setIconImage(new ImageIcon("card-images/icon.png").getImage());
 		
+		//set up game panel
+		this.sPanel = new SolitairePanel();
+		sPanel.setBackground(Color.GREEN);
+		
+		//set up top bar
+		JPanel topBar = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		topBar.setBackground(Color.GREEN);
 		JButton newGameButton = new JButton("New Game");
 		JButton undoButton = new JButton("Undo");
 		JButton redoButton = new JButton("Redo");
-		JLabel timer = new JLabel("Timer: 0 ");
 		
-		settingsBar.add(newGameButton);
-		settingsBar.add(undoButton);
-		settingsBar.add(redoButton);
+		topBar.add(newGameButton);
+		topBar.add(undoButton);
+		topBar.add(redoButton);
+		
+		//set up timer bar
+		JPanel timerBar = new JPanel(new BorderLayout());
+		timerBar.setBackground(Color.GREEN);
+		JLabel timer = new JLabel("Timer: 0 ");
 		timerBar.add(timer, BorderLayout.EAST);
 		
-		frame.add(settingsBar, BorderLayout.NORTH);
+		//add all components to frame and make visible
+		frame.add(sPanel, BorderLayout.CENTER);
+		frame.add(topBar, BorderLayout.NORTH);
 		frame.add(timerBar, BorderLayout.SOUTH);
-
 		frame.setVisible(true);
 	}
 	

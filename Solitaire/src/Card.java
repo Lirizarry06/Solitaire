62410e09264dbb1ecd34d5eb545fbec6ec01ed78
Lirizarry06 <96934863+Lirizarry06@users.noreply.github.com>
2022-04-19 18:56:25 -0600

    Add files via upload

--- Solitaire/src/Card.java
+++ Solitaire/src/Card.java
@@ -1,4 +1,5 @@
 import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,10 +13,11 @@ import javax.imageio.ImageIO;
 
 public class Card {
 
-	public suits Suit;	
+	public suits Suit;		//this card's suit
 	public int number; 		//may be 1-13. Aces are 1. Jacks, Queens, and Kings are 11, 12, and 13.
-	public boolean faceUp = false;		//stores whether or not the card's suit and number are visible
-	public Point location = new Point();		//location of the top-left corner of the card
+	public boolean faceUp = false;			//stores whether or not the card's suit and number are visible
+	public Point location = new Point();	//location of the top-left corner of the card
+	public Polygon area;					//selectable area of card
 	
 	//Enumerates four possible suits.
 	enum suits {
@@ -31,26 +33,17 @@ public class Card {
 		this.number = number;
 	}
 	
-	//Flip card over. If face-up, turn face-down and vice versa.
-	public void flipOver() {
-		faceUp = !faceUp;
-	}
-	
 	//Returns true if this card is a red card
 	public boolean isRed() {
 		return Suit == suits.HEART || Suit == suits.DIAMOND;
 	}
 	
-	public int getNumber() {
-		return number;
-	}
-	
-	public Point getLocation() {
-		return location;
-	}
-	
+	//Updates the location of the card and sets its new selectable area accordingly.
 	public void setLocation(Point newLocation) {
 		this.location = newLocation;
+		int[] x_coords = new int[] {location.x, location.x + 75, location.x + 75, location.x};
+		int[] y_coords = new int[] {location.y, location.y, location.y + 100, location.y + 100};
+		area = new Polygon(x_coords, y_coords, 4);
 	}
 	
 	//Returns the image file of this card
---
+++ Solitaire/src/SelectedCards.java
@@ -0,0 +1,50 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
+/* Program: SelectedCards.java
+ * Date: 18 April 2022
+ * Desc: This class represents the cards currently selected to be moved.
+ */
+
+public class SelectedCards extends ArrayList<Card> {
+
+	private static final long serialVersionUID = -8711653155556335558L;
+	public int[] origIndex;		//original index of the cards in their previous list
+	public ArrayList<Point> locations = new ArrayList<>();	//list of original locations of cards
+	public int deltaX;			//
+	public int deltaY;			//used to calculate offset when updating card location on mouse drag
+	
+	//adds the cards from the given ArrayList to this list and stores their initial locations
+	public void pickUpCards(ArrayList<Card> stack, int[] origIndex) {
+		this.origIndex = origIndex;
+		addAll(stack);
+		for (int i = 0; i < size(); i++) {
+			locations.add(get(i).location);
+		}
+	}
+	
+	//empties this list and reverts all cards' locations to their original locations 
+	public void releaseCards() {
+		if (!isEmpty()) {
+			for (int i = 0; i < size(); i++) {
+				get(i).setLocation(locations.get(i));
+			}
+		}
+		clear();
+	}
+	
+	//changes the locations of the cards in this list according to the given point
+	public void updateLocations(Point p) {
+		for (int i = 0; i < size(); i++) {
+			get(i).location = (new Point(p.x - deltaX, p.y - deltaY + i * 30));
+		}
+	}
+	
+	@Override
+	public void clear() {
+		super.clear();
+		deltaX = 0;
+		deltaY = 0;
+		locations.clear();
+	}
+}
